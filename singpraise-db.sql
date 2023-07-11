5c3a3944de645bc34530042ae5934b9dfa4e7bda
NgatchuDamen <ngatchu.damen@protonmail.com>
2023-07-12 00:06:42 +0100

    sql file v1.1

--- singpraise-db.sql
+++ singpraise-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 07, 2023 at 02:32 AM
+-- Generation Time: Jul 12, 2023 at 12:51 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `singpraise_db`
+-- Database: `singpraise-db`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categories` (
-  `categories_id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `title` varchar(31) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='classification of songs';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,12 +41,12 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `comments` (
-  `comments_id` bigint(20) UNSIGNED NOT NULL,
-  `text` text NOT NULL,
-  `users_id` int(11) NOT NULL,
-  `songs_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `song_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='What people say';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,11 +55,11 @@ CREATE TABLE `comments` (
 --
 
 CREATE TABLE `history` (
-  `history_id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `action` varchar(127) NOT NULL,
-  `users_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tracking who does what with which item';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -68,13 +68,12 @@ CREATE TABLE `history` (
 --
 
 CREATE TABLE `songs` (
-  `songs_id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `author` varchar(31) NOT NULL,
   `musician` varchar(31) NOT NULL,
   `title` varchar(63) NOT NULL,
   `status` varchar(31) NOT NULL,
-  `categories_id` int(11) NOT NULL,
-  `song_items_id` int(11) NOT NULL,
+  `categories_id` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -85,13 +84,14 @@ CREATE TABLE `songs` (
 --
 
 CREATE TABLE `song_items` (
-  `song_items_id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `text` text NOT NULL,
   `sound` mediumblob DEFAULT NULL,
   `picture` blob DEFAULT NULL,
   `video` longblob DEFAULT NULL,
+  `song_id` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Song sheet (pdf)   and  Recording (optional)';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -100,13 +100,21 @@ CREATE TABLE `song_items` (
 --
 
 CREATE TABLE `users` (
-  `users_id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(15) NOT NULL,
   `password` varchar(31) NOT NULL,
   `contact` varchar(90) NOT NULL,
   `role` varchar(15) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Users      Singer (client)     Admin (manages the system)   ';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `contact`, `role`, `created_at`) VALUES
+(1, 'da', '', '', NULL, '2023-07-11 22:48:56'),
+(2, 'Damen', 'damen', 'BP 239 Ouidah\r\nBenin', 'admin', '2023-07-11 22:49:47');
 
 --
 -- Indexes for dumped tables
@@ -116,46 +124,42 @@ CREATE TABLE `users` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`categories_id`),
-  ADD UNIQUE KEY `id` (`categories_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`comments_id`),
-  ADD UNIQUE KEY `id` (`comments_id`),
-  ADD UNIQUE KEY `user_id` (`users_id`),
-  ADD KEY `song_id` (`songs_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `song_id` (`song_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `history`
 --
 ALTER TABLE `history`
-  ADD PRIMARY KEY (`history_id`),
-  ADD UNIQUE KEY `id` (`history_id`),
-  ADD UNIQUE KEY `user_id` (`users_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `songs`
 --
 ALTER TABLE `songs`
-  ADD PRIMARY KEY (`categories_id`,`song_items_id`),
-  ADD UNIQUE KEY `id` (`songs_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categories_id` (`categories_id`);
 
 --
 -- Indexes for table `song_items`
 --
 ALTER TABLE `song_items`
-  ADD PRIMARY KEY (`song_items_id`),
-  ADD UNIQUE KEY `id` (`song_items_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `song_id` (`song_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`users_id`),
-  ADD UNIQUE KEY `id` (`users_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -165,37 +169,66 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `categories_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comments_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `history_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `songs_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `song_items`
 --
 ALTER TABLE `song_items`
-  MODIFY `song_items_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `history`
+--
+ALTER TABLE `history`
+  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `song_items`
+--
+ALTER TABLE `song_items`
+  ADD CONSTRAINT `song_items_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
